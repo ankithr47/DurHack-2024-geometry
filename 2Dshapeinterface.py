34c8b6fb8fa36eaadf4f6fae8b4da92be6e84579
Ankith Rangan <ankithrangan@gmail.com>
2024-11-02 19:01:39 +0000

    shape interface

--- 2Dshapeinterface.py
+++ 2Dshapeinterface.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.patches import Polygon
+from node import *
+
 x_list = []
 y_list = []
+
 
 while True:
     coords = input("enter coordinates (enter 'q' to break): ").split(',')
@@ -10,7 +14,10 @@ while True:
     x_list.append(int(coords[0]))
     y_list.append(int(coords[1]))
 
-plt.scatter(x_list, y_list)
-plt.axvline(x=0, c="black")
-plt.axhline(y=0, c="black")
+points = np.column_stack((x_list, y_list))
+p = Polygon(points)
+ax = plt.gca()
+ax.add_patch(p)
+ax.set_xlim(min(x_list)-1, max(x_list)+1)
+ax.set_ylim(min(y_list)-1, max(y_list)+1)
 plt.show()
