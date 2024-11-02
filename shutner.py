93ec93eb2da9cfe9b03102b71ddc598f02109ffb
Thomas Blair <tomblair3008@yahoo.com>
2024-11-02 15:23:20 +0000

    Deleted stuff.py and made a FINISHED stack.py file

--- shutner.py
+++ shutner.py
@@ -1,2 +1,22 @@
-def evaluate():
-    return True
+from stack import *
+def toPostfix(infix):
+    return infix
+def evaluate(expression):
+    a = Stack()
+    operators = {'*','/','+','-'}
+    for i in range(0,len(expression)):
+        if expression[i] in operators:
+            num1 = a.pop()
+            num2 = a.pop()
+            if expression[i] == '*':
+                a.push(num1*num2)
+            elif expression[i] == '/':
+                a.push(num2/num1)
+            elif expression[i] == '+':
+                a.push(num1+num2)
+            else:
+                a.push(num2-num1)
+        else:
+            a.push(int(expression[i]))
+    return a.pop()
+print(evaluate("15+15+*"))
