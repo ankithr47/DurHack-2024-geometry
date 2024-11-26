f439da60563f9e78462bae0120f838c82e453fbf
Ankith Rangan <ankithrangan@gmail.com>
2024-11-26 21:21:38 +0000

    changed wording of readme file

--- 2Dshapeinterface.py
+++ 2Dshapeinterface.py
@@ -13,27 +13,27 @@ while count < 4:
     y_list.append(int(coords[1]))
     count += 1
 
-#order coordinates so that a quadrilateral can be drawn between the points
+# order coordinates so that a quadrilateral can be drawn between the points
 x_new_lst, y_new_lst = node.order(x_list, y_list)
 
-#create the shape from the points
+# create the shape from the points
 points = np.column_stack((x_new_lst, y_new_lst))
 p = Polygon(points)
 ax = plt.gca()
 
-#draw the shape on the axes
+# draw the shape on the axes
 ax.add_patch(p)
 
-#set limit for the numbers displayed on the x and y axis
+# set limit for the numbers displayed on the x and y axis
 ax.set_xlim(min(x_list)-1, max(x_list)+1)
 ax.set_ylim(min(y_list)-1, max(y_list)+1)
 
-#draw the x and y axes on the graph
+# draw the x and y axes on the graph
 plt.axvline(x=0, c="black")
 plt.axhline(y=0, c="black")
 
-#display the text showing the area underneath the shape
+# display the text showing the area underneath the shape
 plt.text(x=sum(x_list)/len(x_list), y=min(y_list)-0.5, s=(f'area = {round(node.area(x_list,y_list), 2)}'))
-#show the plot
+# show the plot
 plt.show()
 
--- node.py
+++ node.py
@@ -2,29 +2,35 @@ import math
 from itertools import permutations
 import shapely.geometry
 
+# create a class for each vertex of the shape
 class node:
     
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    # method to find distance between 2 nodes(vertices)
     def distanceFrom(self,other):
         return math.sqrt((other.x - self.x)**2+(other.y - self.y)**2)
     
+    # method to calculate total distance between a given path of 4 nodes
+    # answer depends on list order 
     def total_distance(nodelist):
         totalDistance = 0
         for i in range(len(nodelist)):
             totalDistance += nodelist[i].distanceFrom(nodelist[(i+1)%len(nodelist)]) #wrap around
         return totalDistance
     
+    ''' method to reorder the coordinates such that drawing a line between each 
+        coordinate in the new order will produce the quadrilateral'''
     def order(xlist,ylist ):
-        #create nodes from given coordinates
+        # create nodes from given coordinates
         nodes = [node(x,y) for x, y in zip(xlist, ylist)]
             
-        #generate all permutations of nodes
+        # generate all permutations of nodes
         node_permutations = permutations(nodes)
 
-        #find permutation with smallest total distance
+        # find permutation with smallest total distance
         min_distance = float('inf')
         best_order = None
 
@@ -34,37 +40,40 @@ class node:
                 min_distance = distance
                 best_order = i
 
-        #extract ordered x and y coordinates
+        # extract ordered x and y coordinates
         order_x = [node.x for node in best_order]
         order_y = [node.y for node in best_order]
 
         return order_x, order_y
     
+    # method to find area of quadrilateral
     def area(xlist,ylist):
+        # convert xlist and ylist into a single list of coordinates (nodelist)
         nodelist = []
         for i in range(0,len(xlist)):
             nodelist.append(node(xlist[i],ylist[i]))
-        s1 = node.total_distance([nodelist[0],nodelist[1],nodelist[2]])/2
-        s2 = node.total_distance([nodelist[0],nodelist[3],nodelist[2]])/2
-        p1 = math.sqrt(s1*(s1-nodelist[0].distanceFrom(nodelist[1]))*(s1-nodelist[1].distanceFrom(nodelist[2]))*(s1-nodelist[2].distanceFrom(nodelist[0])))
-        p2 = math.sqrt(s2*(s2-nodelist[0].distanceFrom(nodelist[3]))*(s2-nodelist[3].distanceFrom(nodelist[2]))*(s2-nodelist[2].distanceFrom(nodelist[0])))
-        return p1 + p2
+        # split quadrilateral into 2 triangles and use Heron's formula to compute the area of each triangle
+        s1 = node.total_distance([nodelist[0],nodelist[1],nodelist[2]])/2   # s1 = semiperimeter of triangle 1
+        s2 = node.total_distance([nodelist[0],nodelist[3],nodelist[2]])/2   # s2 = semiperimeter of triangle 2 
+        a1 = math.sqrt(s1*(s1-nodelist[0].distanceFrom(nodelist[1]))*(s1-nodelist[1].distanceFrom(nodelist[2]))*(s1-nodelist[2].distanceFrom(nodelist[0]))) # a1 = area of triangle 1
+        a2 = math.sqrt(s2*(s2-nodelist[0].distanceFrom(nodelist[3]))*(s2-nodelist[3].distanceFrom(nodelist[2]))*(s2-nodelist[2].distanceFrom(nodelist[0]))) # a2 = area of triangle 2
+        return a1 + a2
 
-    #the function below doesn't quite work yet, its aim is to find the area
-    #of any regular/irregular polygon by splitting it up into triangles
-    #and computing the area of each triangle using Heron's formula
+    '''the function below doesn't quite work yet. Its aim is to find the area
+    of any regular/irregular polygon by splitting it up into triangles
+    (via an algorithm shown on polygonareapseudo.txt) and computing the area of each triangle using Heron's formula'''
 
     def total_area(xlist, ylist):
-        #create list of x, y nodes
+        # create list of x, y nodes
         nodes = [node(x,y) for x,y in zip(xlist, ylist)]
-        #create polygon
+        # create polygon
         polypoints =[(xlist[i], ylist[i]) for i in range(len(xlist))]
         polygon = shapely.geometry.Polygon(polypoints)
         i = 0
         tot_area = 0
         while len(nodes) > 3:
             test_coord = shapely.geometry.Point((nodes[i % len(nodes)].x + nodes[(i+2) % len(nodes)].x)/2, nodes[i % len(nodes)].y + nodes[(i+2) % len(nodes)].y/2)
-            #check if test coordinate is inside the polygon, if so, find area of the triangle
+            # check if test coordinate is inside the polygon, if so, find area of the triangle
             if test_coord.within(polygon):
                 s = node.total_distance([nodes[i % len(nodes)], nodes[(i+1) % len(nodes)], nodes[(i+2) % len(nodes)]]) / 2
                 area = math.sqrt(s*(s-nodes[i % len(nodes)].distanceFrom(nodes[(i+1) % len(nodes)]))*(s-nodes[(i+1) % len(nodes)].distanceFrom(nodes[(i+2) % len(nodes)]))*(s-nodes[(i+2) % len(nodes)].distanceFrom(nodes[i % len(nodes)])))
